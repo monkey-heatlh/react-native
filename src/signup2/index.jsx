59c05298550fdb757673d6880279b8b82afe06fe
bae080311 <s24024@gsm.hs.kr>
2024-12-26 23:54:51 +0900

    Feat: signup 2

--- src/signup2/index.jsx
+++ src/signup2/index.jsx
@@ -1,5 +1,68 @@
-import { View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { style } from "./style";
+import PurpleBTn from "../components/purpleBtn";
+import { useState } from "react";
+import axios from "axios";
+import { url } from "../../config";
+import { useNavigation } from "@react-navigation/native";
 
-export default function SignUp2() {
-  return <View></View>;
+export default function SignUp2({ email }) {
+  const navigation = useNavigation();
+  function signUp() {
+    if (error) return;
+    axios
+      .post(`${url}/signup`, {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        navigation.navigate("login");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  const [error, setError] = useState(false);
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  return (
+    <View style={style.Container}>
+      <Text style={style.title}>비밀번호를 설정해주세요.</Text>
+      <Text>비밀번호를 설정하여 나만의 계정을 보호하세요!</Text>
+      <View style={style.inputWrap}>
+        <Text style={style.label}>비밀번호</Text>
+        <TextInput
+          onChangeText={(text) => {
+            setPassword(text);
+            setError(false);
+          }}
+          placeholder="비밀번호를 입력해주세요"
+          style={styles.input}
+        />
+      </View>
+      <View style={style.inputWrap}>
+        <Text style={style.label}>비밀번호 확인</Text>
+        <TextInput
+          onChangeText={(text) => {
+            setPasswordCheck(text);
+            setError(password !== text);
+          }}
+          placeholder="비밀번호를 재입력해주세요"
+          style={{
+            ...styles.input,
+            borderColor: error ? "#FF0000" : "#A6A6A6",
+          }}
+        />
+      </View>
+      <PurpleBTn label="확인" onPress={signUp} Btnstyle={error} />
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    padding: 16,
+    borderRadius: 8,
+    borderColor: "#A6A6A6",
+  },
+});
---
+++ src/signup2/style.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+  Container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    justifyContent: "start",
+    paddingTop: 100,
+    padding: 20,
+    gap: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  warn: {
+    color: "#A6A6A6",
+    fontSize: 14,
+    marginTop: 4,
+  },
+  inputWrap: {
+    gap: 5,
+  },
+  label: {
+    fontSize: 14,
+  },
+});
