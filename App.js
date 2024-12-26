da1aac71db94609824c91adac08c7dcfb94c9449
bae080311 <s24024@gsm.hs.kr>
2024-12-26 20:20:13 +0900

    Feat: axios routing

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Start from "./src";
+import SignUp from "./src/signup";
+import SignUp2 from "./src/signup2";
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -10,6 +12,16 @@ export default function App() {
         <Stack.Screen
           name="start"
           component={Start}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="signup"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="signup2"
+          component={SignUp2}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
