7566279fa0d7fb65dca124a637b1085c09798481
bae080311 <s24024@gsm.hs.kr>
2024-12-26 18:02:36 +0900

    Feat: stating page

---
+++ src/index.jsx
@@ -0,0 +1,44 @@
+import { Image, StyleSheet, View } from "react-native";
+import PurpleBtn from "./components/purpleBtn";
+import { useNavigation } from "@react-navigation/native";
+import WhiteBtn from "./components/whiteBtn";
+
+export default function Start() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.Wrapper}>
+      <Image
+        style={styles.Logo}
+        resizeMode="contain"
+        source={require("./images/textlogo.png")}
+      />
+      <View style={styles.BtnWrapper}>
+        <WhiteBtn
+          label="회원가입"
+          onPress={() => navigation.navigate("/signup")}
+        />
+        <PurpleBtn
+          label="로그인"
+          onPress={() => navigation.navigate("/login")}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  Wrapper: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  Logo: {
+    width: 260,
+    height: 42,
+    flex: 0.8,
+  },
+  BtnWrapper: {
+    flexDirection: "column",
+    gap: 12,
+  },
+});
