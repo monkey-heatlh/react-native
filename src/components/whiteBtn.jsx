93501accc475de209fbdb7de94230edef5b112fc
bae080311 <s24024@gsm.hs.kr>
2024-12-26 18:02:00 +0900

    Feat: white button component

---
+++ src/components/whiteBtn.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+export default function WhiteBtn({ label, onPress }) {
+  return (
+    <TouchableOpacity style={styles.PurpleBtn} onPress={onPress}>
+      <Text style={styles.PurpleBtnLabel}>{label}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  PurpleBtn: {
+    paddingHorizontal: 134,
+    paddingVertical: 16,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FFFFFF",
+    borderWidth: 1,
+    borderColor: "#902BE9",
+  },
+  PurpleBtnLabel: {
+    color: "#902BE9",
+    fontSize: 16,
+  },
+});
