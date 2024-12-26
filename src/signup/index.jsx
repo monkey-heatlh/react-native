04809469b6176c1501df0fb6daf082848a5dac1e
bae080311 <s24024@gsm.hs.kr>
2024-12-26 20:16:22 +0900

    Feat: check certification

--- src/signup/index.jsx
+++ src/signup/index.jsx
@@ -6,11 +6,15 @@ import DisBtn from "../components/disBtn";
 import axios from "axios";
 import { url } from "../../config";
 import { useNavigation } from "@react-navigation/native";
+import PurpleBtn from "../components/purpleBtn";
 
 export default function SignUp() {
   const [active, setActive] = useState(false);
   const [email, setEmail] = useState("");
-  const [focus, setFocus] = useState(false);
+  const [error, setError] = useState(false);
+  const [next, setNext] = useState(true);
+  const [certification, setCertification] = useState("");
+  const [Btnstyle, setStyle] = useState(false);
 
   const navigation = useNavigation();
   const emailRegex =
@@ -25,6 +29,23 @@ export default function SignUp() {
       .post(`${url}/send-code`, {
         email: email,
       })
+      .then((res) => {
+        setNext(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function check() {
+    axios
+      .post(`${url}/check-code`, {
+        email: email,
+        code: certification,
+      })
+      .then((res) => {
+        navigation.navigate("signup2");
+      })
       .catch((err) => {
         console.log(err);
       });
@@ -37,19 +58,34 @@ export default function SignUp() {
       <TextInput
         style={{
           ...styles.input,
-          borderColor: focus ? "#902BE9" : "#A6A6A6",
+          borderColor: error ? "#902BE9" : "#A6A6A6",
         }}
         placeholder="이메일을 입력해주세요"
         value={email}
-        onChangeText={setEmail}
-        onFocus={() => setFocus(true)}
-        onBlur={() => setFocus(false)}
+        onChangeText={(text) => {
+          setEmail(text);
+          setActive(emailRegex.test(text));
+        }}
       />
       {active ? (
         <WhiteBtn label={"인증번호 전송"} onPress={sendCertification} />
       ) : (
         <DisBtn label={"인증번호 전송"} />
       )}
+      <View>
+        {next && (
+          <TextInput
+            onChangeText={(e) => {
+              setCertification(e.target.value);
+              setStyle(true);
+            }}
+            placeholder="인증번호 입력"
+            style={styles.input}
+          />
+        )}
+        {next && <Text style={style.warn}>타인에게 공유하면 위험해요!!</Text>}
+      </View>
+      <PurpleBtn style={Btnstyle} label={"동의하고 시작하기"} onPress={check} />
     </View>
   );
 }
@@ -59,5 +95,6 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 16,
     borderRadius: 8,
+    borderColor: "#A6A6A6",
   },
 });
