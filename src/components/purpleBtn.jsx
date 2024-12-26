3aac2595d1e3638d9f74900a0124a1a32f87b341
bae080311 <s24024@gsm.hs.kr>
2024-12-26 20:15:11 +0900

    Fix: purple Btn style

--- src/components/purpleBtn.jsx
+++ src/components/purpleBtn.jsx
@@ -1,24 +1,25 @@
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
-export default function PurpleBtn({ label, onPress }) {
+export default function PurpleBtn({ label, onPress, Btnstyle }) {
+  const styles = StyleSheet.create({
+    PurpleBtn: {
+      backgroundColor: Btnstyle ? "#902BE9" : "#690ED4",
+      paddingHorizontal: "auto",
+      paddingVertical: 16,
+      borderRadius: 10,
+      alignItems: "center",
+      justifyContent: "center",
+      width: "100%",
+    },
+    PurpleBtnLabel: {
+      color: Btnstyle ? "#FFFFFF" : "#C5C5C5",
+      fontSize: 16,
+    },
+  });
+
   return (
     <TouchableOpacity style={styles.PurpleBtn} onPress={onPress}>
       <Text style={styles.PurpleBtnLabel}>{label}</Text>
     </TouchableOpacity>
   );
 }
-
-const styles = StyleSheet.create({
-  PurpleBtn: {
-    backgroundColor: "#902BE9",
-    paddingHorizontal: 140,
-    paddingVertical: 16,
-    borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  PurpleBtnLabel: {
-    color: "#FFFFFF",
-    fontSize: 16,
-  },
-});
