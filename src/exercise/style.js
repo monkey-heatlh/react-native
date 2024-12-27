07f4f7033f12e2b7d5d552bba9483986252426d6
bae080311 <s24024@gsm.hs.kr>
2024-12-27 10:10:23 +0900

    Feat: style

--- src/exercise/style.js
+++ src/exercise/style.js
@@ -3,14 +3,19 @@ import { StyleSheet } from "react-native";
 export const style = StyleSheet.create({
   Container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
     alignContent: "center",
     padding: 25,
+    gap: 40,
   },
   title: {
     fontSize: 30,
     fontWeight: 600,
+  },
+  titleWrap: {
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
   },
   subTitle: {
     fontSize: 22,
@@ -21,5 +26,16 @@ export const style = StyleSheet.create({
     flexWrap: "wrap",
     justifyContent: "left",
     gap: 20,
+  },
+  goBack: {
+    flexDirection: "row",
+    justifyContent: "left",
+    alignItems: "center",
+    marginTop: 30,
+    marginRight: "auto",
+  },
+  icon: {
+    width: 24,
+    height: 24,
   },
 });
