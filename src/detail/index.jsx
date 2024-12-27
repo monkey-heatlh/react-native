d634c2d10b8b15d23051876ad3d25a2635aeeb65
bae080311 <s24024@gsm.hs.kr>
2024-12-27 10:57:20 +0900

    Feat: detail

---
+++ src/detail/index.jsx
@@ -0,0 +1,28 @@
+import { Image, Text, View } from "react-native";
+import GoBack from "../components/gaback";
+import { style } from "./style";
+import PurpleBtn from "../components/purpleBtn";
+
+export default function Detail({ exercise }) {
+  const Bool = exercise === "가슴";
+  return (
+    <View style={style.Container}>
+      <GoBack />
+      <Text style={style.title}>{Bool ? "PUSH UP" : "FLANK"}</Text>
+      <Text>
+        {Bool
+          ? `운동시 대흉근, 삼두근, 전면/측면삼각근\n복근, 전거근, 광배근 등의 근육군이 쓰인다.`
+          : `등척성 코어 근육 운동으로 근지구렬 향상에 도움을 주며 \n늑골, 척추, 골반을 연결하고 몸을 바로세우는 \n근육을 단련하여 자세 교정에 도움을 준다.`}
+      </Text>
+      <Image
+        style={style.img}
+        source={
+          Bool
+            ? require("../images/pushUp.png")
+            : require("../images/flank.png")
+        }
+      />
+      <PurpleBtn label={"바로 시작"} Btnstyle={true} />
+    </View>
+  );
+}
---
+++ src/detail/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+  img: {
+    width: "100%",
+    height: 370,
+    marginBottom: 30,
+  },
+  Container: {
+    flex: 1,
+    padding: 20,
+    gap: 30,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 600,
+  },
+});
