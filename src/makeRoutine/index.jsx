932b64e52350f7a600f167f08860940891f64766
bae080311 <s24024@gsm.hs.kr>
2024-12-29 13:06:11 +0900

    Feat: pre next 버튼 Fix: value

--- src/makeRoutine/index.jsx
+++ src/makeRoutine/index.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import RNPickerSelect from "react-native-picker-select";
 import { Ionicons } from "@expo/vector-icons";
+import PurpleBtn from "../components/purpleBtn";
+import WhiteBtn from "../components/whiteBtn";
 
 const days = {
   월: "",
@@ -9,26 +11,44 @@ const days = {
   수: "",
   목: "",
   금: "",
-  토: "",
-  일: "",
 };
 
 export default function MakeRoutine() {
-  const [selectedValue, setSelectedValue] = useState(days);
+  const [selectedValues, setSelectedValues] = useState(days);
+  const [currentDay, setCurrentDay] = useState("월");
+
+  const handleValueChange = (value) => {
+    if (value) {
+      setSelectedValues((prev) => ({
+        ...prev,
+        [currentDay]: value,
+      }));
+    }
+  };
+  const handlePrevDay = () => {
+    const prevDayIndex = Object.keys(days).indexOf(currentDay) - 1;
+    if (prevDayIndex >= 0) {
+      setCurrentDay(Object.keys(days)[prevDayIndex]);
+    }
+  };
+  const handleNextDay = () => {
+    const nextDayIndex = Object.keys(days).indexOf(currentDay) + 1;
+    if (nextDayIndex < Object.keys(days).length) {
+      setCurrentDay(Object.keys(days)[nextDayIndex]);
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text style={styles.label}>{currentDay}</Text>
       <RNPickerSelect
-        onValueChange={(value) => {
-          if (value) {
-            setSelectedValue(value);
-          }
-        }}
+        onValueChange={handleValueChange}
         items={[
-          { label: "운동 루틴", value: "exercise" },
-          { label: "공부 루틴", value: "study" },
-          { label: "취미 루틴", value: "hobby" },
+          { label: "가슴", value: "가슴" },
+          { label: "등", value: "등" },
+          { label: "하체", value: "하체" },
+          { label: "복근", value: "복근" },
+          { label: "전신", value: "전신" },
         ]}
         placeholder={{
           label: "운동부위 선택",
@@ -47,6 +67,12 @@ export default function MakeRoutine() {
           />
         )}
       />
+      <WhiteBtn label="뒤로" onPress={handlePrevDay} />
+      <PurpleBtn
+        label={currentDay === "금" ? "확인" : "다음"}
+        onPress={handleNextDay}
+        Btnstyle={true}
+      />
     </View>
   );
 }
@@ -56,10 +82,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     padding: 20,
+    alignItems: "center",
+    gap: 20,
   },
   label: {
     fontSize: 16,
-    marginBottom: 10,
+    marginVertical: 10,
   },
   input: {
     fontSize: 16,
@@ -67,11 +95,7 @@ const styles = StyleSheet.create({
     borderColor: "#777",
     padding: 10,
     borderRadius: 5,
-    paddingRight: 30, // 아이콘 공간 확보
-  },
-  selectedText: {
-    marginTop: 20,
-    fontSize: 16,
+    paddingRight: 30,
   },
   icon: {
     position: "absolute",
