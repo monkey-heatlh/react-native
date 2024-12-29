7a053d0593d7c3dc06ba54763d618a3b143e64a2
bae080311 <s24024@gsm.hs.kr>
2024-12-29 22:37:54 +0900

    Feat: routing

--- App.js
+++ App.js
@@ -3,6 +3,14 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Start from "./src";
 import SignUp from "./src/signup";
 import SignUp2 from "./src/signup2";
+import Login from "./src/login";
+import Main from "./src/main";
+import "./App.css";
+import Exercise from "./src/exercise";
+import Detail from "./src/detail";
+import CameraScreen from "./src/camera";
+import MakeRoutine from "./src/makeRoutine";
+import CalendarScreen from "./src/calender";
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -10,21 +18,56 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="start"
-          component={Start}
+          name="login"
+          component={Login}
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name="signup"
-          component={SignUp}
+          name="calender"
+          component={CalendarScreen}
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name="signup2"
-          component={SignUp2}
+          name="makeRoutine"
+          component={MakeRoutine}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="camera"
+          component={CameraScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="detail"
+          component={Detail}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="exercise"
+          component={Exercise}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="main"
+          component={Main}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
+      <Stack.Screen
+        name="signup2"
+        component={SignUp2}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="start"
+        component={Start}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="signup"
+        component={SignUp}
+        options={{ headerShown: false }}
+      />
     </NavigationContainer>
   );
 }
