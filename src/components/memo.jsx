5fec663c1fda768bd9dc6197cce83d29f491da50
bae080311 <s24024@gsm.hs.kr>
2024-12-30 09:14:44 +0900

    Fix: memo

--- src/components/memo.jsx
+++ src/components/memo.jsx
@@ -1,20 +1,26 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Memo(memo) {
+export default function Memo({ memo }) {
   return (
     <View style={styles.container}>
-      <Text>{memo.content}</Text>
+      <Text style={styles.memoText}>{memo.content || "불러오기 실패"}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     padding: 20,
     backgroundColor: "#fff",
     borderTopWidth: 1,
     borderTopColor: "#D8d8d8",
     paddingVertical: 24,
+    height: "auto",
+    width: "100%",
+  },
+  memoText: {
+    fontSize: 16,
+    color: "black",
+    flexShrink: 1,
   },
 });
