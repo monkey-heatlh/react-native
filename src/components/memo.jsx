bf5a1be96813d3f1a1633e2317faf2511249c39e
bae080311 <s24024@gsm.hs.kr>
2024-12-30 09:19:06 +0900

    feat: delete icon

--- src/components/memo.jsx
+++ src/components/memo.jsx
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function Memo({ memo }) {
   return (
     <View style={styles.container}>
       <Text style={styles.memoText}>{memo.content || "불러오기 실패"}</Text>
+      <Image source={require("../images/delete.png")} style={styles.icon} />
     </View>
   );
 }
@@ -17,10 +18,16 @@ const styles = StyleSheet.create({
     paddingVertical: 24,
     height: "auto",
     width: "100%",
+    justifyContent: "space-between",
+    flexDirection: "row",
   },
   memoText: {
     fontSize: 16,
     color: "black",
     flexShrink: 1,
+  },
+  icon: {
+    width: 24,
+    height: 24,
   },
 });
